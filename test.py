c55a5b6899d9758cbccca0c96c649802937f7560
SBor62 <velisavres@gmail.com>
2025-01-17 18:45:48 +0400

    add test.py
    изменение программы

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-print("Не тест")
-numbers = 100
+# Банк
+
+def bank(a, years):
+    for _ in range(years):
+        a += a * 0.10  # Увеличиваем вклад на 10%
+
+    return a
+
+# Ввод данных
+deposit = float(input("Введите сумму вклада: "))
+num_years = int(input("Введите срок вклада в годах: "))
+
+result = bank(deposit, num_years)
+
+print(f"Сумма на счету через {num_years} лет: {result} ")
