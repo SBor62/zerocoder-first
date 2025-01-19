c81c8d2975cf5615422730ac36ff3b11ea60212b
SBor62 <velisavres@gmail.com>
2025-01-19 20:14:09 +0400

    Update Animal.py
    добавлены пояснения в программу

--- Animal.py
+++ Animal.py
@@ -1,33 +1,37 @@
-class Animal():    #
-    def __init__(self, name, age):  #
+class Animal():    # Создание класса Животные
+    def __init__(self, name, age):  # Общие характеристики животных
         self.name = name
         self.age = age
-    def eat(self):   #
+
+    def eat(self):    # Общие функции животных
         print(f"{self.name} ест")
-    def make_sound(self):   #
+
+    def make_sound(self):
         pass
 
-class Bird(Animal):    #
+
+class Bird(Animal):    # Создание производного класса птиц
     def make_sound(self):
         print("кря кря")
 
 
-class Mammal(Animal):    #
+class Mammal(Animal):    # Создание производного класса млекопитающих
     def make_sound(self):
         print("гав")
 
 
-class Reptile(Animal):    #
+class Reptile(Animal):    # Создание производного класса рептилий
     def make_sound(self):
         print("шшш-ссс")
 
-def animal_sound(animals):    #
+
+def animal_sound(animals):    # демонстрация полиморфизма - одна функция для всех животных
     for animal in animals:
         animal.make_sound()
 
 
-class Zoo():   #
-    def __init__(self):  #
+class Zoo():   # Создание независимого класса Зоопарк
+    def __init__(self):  # Создание списков животных и сотрудников
         self.animals = []
         self.staff = []
 
@@ -40,29 +44,28 @@ class Zoo():   #
         print(f"Сотрудник {new_staff} добавлен в персонал зоопарка")
 
 
-class ZooKeeper():   #
+class ZooKeeper():   # Содание класса сотрудников
     def feed_animal(self, animal):
         print(f"Сотпудник кормит {animal.name}")
 
 
-
-class Veterinarian():   #
+class Veterinarian():   # Создание класса ветеринаров
     def heal_animal(self, animal):
         print(f"Ветеринар лечит {animal.name}")
 
-#
+# Создание объектов классов животных
 bird1 = Bird(name="Утка", age= 1)
 mammal1 = Mammal(name= "Собака", age= 3)
 reptile1 = Reptile(name= "Удав", age= 5)
 
-#
+# Создание объекта Зоопарк
 zoo = Zoo()
 
-#
+# Создание объектов сотрудников
 keeper = ZooKeeper()
 veterinarian = Veterinarian()
 
-#
+# Добавление животных и сотрудников в зоопарк
 zoo.add_animal(bird1)
 zoo.add_animal(mammal1)
 zoo.add_animal(reptile1)
@@ -70,9 +73,9 @@ zoo.add_animal(reptile1)
 zoo.add_staff(keeper)
 zoo.add_staff(veterinarian)
 
-#
+# Вызов функции animal_sound для всех животных зоопарка
 animal_sound(zoo.animals)
 
-#
+# Вызов функций для сотрудников зоопарка
 keeper.feed_animal(mammal1)
 veterinarian.heal_animal(bird1)
